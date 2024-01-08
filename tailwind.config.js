00cf2cb2cbd3cd1317a02e097de240b2661d06c8
mrK <mrKDev@gmail.com>
2024-01-08 16:05:25 +0700

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  darkMode: 'class',
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'],
+      borderColor: ['dark'],
+      textColor: ['dark'],
+    },
+  },
+  plugins: []
+}
